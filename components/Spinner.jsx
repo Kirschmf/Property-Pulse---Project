8b8a49219aa7823fc8f40055ea409314ef3e37f9
Matheus Kirsch <makfonseca@gmail.com>
2025-10-02 15:29:32 -0300

    Created google client & client secret to oAuth

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -6,7 +6,7 @@ const override = {
     margin: '100px auto',
 }
 
-const LoadingPage = ({ loading }) => {
+const Spinner = ({ loading }) => {
   return (
     <ClipLoader
         color='#3b82f6'
@@ -15,7 +15,7 @@ const LoadingPage = ({ loading }) => {
         size={150}
         aria-label="Loading Spinner"
       />
-  )
-}
+  );
+};
 
-export default LoadingPage
+export default Spinner;
